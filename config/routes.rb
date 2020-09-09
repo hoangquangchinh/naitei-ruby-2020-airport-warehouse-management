ed50029b522aa14d602070a8c5cf583ce5d12d7c
chienpt98 <pham.thi.chien@sun-asterisk.com>
2020-09-09 09:13:52 +0700

    task/27039-edit-profile

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to:"sessions#destroy"
-    resources :users, except: %i(edit update)
+    resources :users
     resources :requests
     resources :locations, only: :index
     resources :planes, only: %i(index show)
